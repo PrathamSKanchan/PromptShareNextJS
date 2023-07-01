53ac2339ddd680832df711b97ec1dd94f18eb823
Pratham <prathamskanchan@gmail.com>
2023-07-01 21:13:33 +0530

    dev: initial commit

---
+++ promptshare/app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/global.css";
+
+export const metadata = {
+  title: "propmtshare",
+  description: "Share your AI prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <main className="app">{children}</main>
+    </body>
+  </html>
+);
+
+export default RootLayout;
