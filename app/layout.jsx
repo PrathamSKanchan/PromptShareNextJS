35499ef60c1da6b33849009c3cba5147a2868373
Pratham <prathamskanchan@gmail.com>
2023-07-17 22:24:33 +0530

    dev: created required files and basic next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,29 @@
 import "@styles/global.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "propmtshare",
   description: "Share your AI prompts",
 };
 
-const RootLayout = ({ children }) => (
-  <html lang="en">
-    <body>
-      <div className="main">
-        <div className="gradient" />
-      </div>
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-      <main className="app">{children}</main>
-    </body>
-  </html>
-);
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
